151de1090738e6f6ac56d79701205e77e36b4834
Marcel Wiessler <marcel@gaisterhand.de>
2022-07-07 16:05:37 +0200

    Css track: editor track name fix and add css icon

--- package/TimelineHtml/Editor/CssTrackEditor.cs
+++ package/TimelineHtml/Editor/CssTrackEditor.cs
@@ -1,13 +1,30 @@
 ﻿using JetBrains.Annotations;
+using UnityEditor;
 using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.Timeline;
 
 namespace TimelineHtml.Editor
 {
+
+	[CustomTimelineEditor(typeof(CssTrack))]
+	[UsedImplicitly]
+	public class CssTrackEditor : TrackEditor
+	{
+		private Texture2D icon;
+		
+		public override TrackDrawOptions GetTrackOptions(TrackAsset track, Object binding)
+		{
+			var opts = base.GetTrackOptions(track, binding);
+			if (!icon) icon = AssetDatabase.LoadAssetAtPath<Texture2D>(AssetDatabase.GUIDToAssetPath("297f5203957bd664b8f034858162ccda"));
+			opts.icon = icon;
+			return opts;
+		}
+	}
+
 	[CustomTimelineEditor(typeof(CssPlayableAsset))]
 	[UsedImplicitly]
-	public class VideoAssetClipEditor : ClipEditor
+	public class CssClipEditor : ClipEditor
 	{
 		public override void DrawBackground(TimelineClip clip, ClipBackgroundRegion region)
 		{
