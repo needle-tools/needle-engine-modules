3fcf2f6ba3e811a906bfa245fd23cecca6a86deb
Marcel Wiessler <marcel@gaisterhand.de>
2022-08-08 22:35:34 +0200

    Drive codegen

--- package/GoogleDrive/GoogleDrive.codegen/DriveClient.cs
+++ package/GoogleDrive/GoogleDrive.codegen/DriveClient.cs
@@ -1,3 +1,4 @@
+// NEEDLE_CODEGEN_START
 // auto generated code - do not edit directly
 
 #pragma warning disable
@@ -12,23 +13,9 @@ namespace Needle.Typescript.GeneratedComponents
 		public bool @useFilePicker = false;
 		public void onEnable(){}
 		public void init(object @gapi, object @google){}
-		// Could not resolve type "DriveUIComponent"
-		// private undefined @_ui;
-		// Could not resolve type "DriveApi"
-		// private undefined @_api;
-		// Could not resolve type "DriveFilePicker"
-		// private undefined @picker;
-		// Could not resolve type "DriveNetworking"
-		// private undefined @_networking;
-		// Could not resolve type "DriveFileAccess"
-		// private undefined @files;
-		// Could not resolve type "DriveModelFileManager"
-		// private undefined @modelManager;
-		// Could not resolve type "DriveShareDialogue"
-		// private undefined @shareClient;
 		public void handleAuthClick(){}
-		// Could not resolve type "FileCancellation"
-		// private undefined @currentCancellationToken;
 		public void update(){}
 	}
 }
+
+// NEEDLE_CODEGEN_END
