ad628c4894d30458e08e72b96e5c3de875dfcae2
hybridherbst <felix.herbst@gmail.com>
2023-10-20 16:23:00 +0200

    cleanup

--- package/Pathtracing/Needle.Pathtracing~/codegen/register_types.ts
+++ package/Pathtracing/Needle.Pathtracing~/codegen/register_types.ts
@@ -1,8 +1,8 @@
 ﻿import { TypeStore } from "@needle-tools/engine"
 
 // Import types
-import { CameraTracker } from "../CameraTracker";
-import { Pathtracing } from "../Pathtracing";
+import { CameraTracker } from "../CameraTracker.js";
+import { Pathtracing } from "../Pathtracing.js";
 
 // Register types
 TypeStore.add("CameraTracker", CameraTracker);
